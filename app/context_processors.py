1bc51c3b3d9167e4ff5ba55715e929d2404c6f49
Dariashariko1990 <Ganesh0192>
2020-07-21 21:09:33 +0300

    Context processor updated.

--- app/context_processors.py
+++ app/context_processors.py
@@ -2,9 +2,9 @@ from .models import Category
 
 
 def categories(request):
+    accessories = Category.objects.filter(name='Аксессуары').prefetch_related('subcategory')
     categories = Category.objects.exclude(name='Аксессуары').exclude(name='Одежда').prefetch_related('subcategory')
-    accessories = Category.objects.filter(name='Аксессуары').prefetch_related('subcategory')[0]
-    clothes = Category.objects.get(name='Одежда')
+    clothes = Category.objects.filter(name='Одежда').prefetch_related('subcategory')
 
     return {'categories': categories,
             'accessories': accessories,
